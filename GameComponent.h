86f8d3eb1fc9b8ff209002a76195b0b09f57fd66
thereconjacob <jvpokeyjake@gmail.com>
2023-03-14 14:01:06 +0000

    Populate gamecomponent

--- GameComponent.h
+++ GameComponent.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <ctime>
+#include <iostream>
 class GameComponent
 {
 private:
 	int id;
-	int instances;
+	int instances = 0;
 
 public:
 	GameComponent()
 	{
-
+		instances++;
+		id = instances;
 	}
 
 	void Update(const tm* invokedTime)
 	{
-
+		std::cout << "ID: " << id;
+		std::cout << "Time of Invocation: " << instances;
 	}
 };
 
